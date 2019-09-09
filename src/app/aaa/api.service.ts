183190e737c3992af88ed673481d9d10c2fc5ec0
Alexey Popov <asdalexey@yandex.ru>
2019-09-09 23:16:02 +0300

    fix https ssr errors

--- src/app/aaa/api.service.ts
+++ src/app/aaa/api.service.ts
@@ -6,7 +6,9 @@ import { Observable } from 'rxjs';
 export class UserService {
   constructor(private http: HttpClient) {}
 
+  // https://github.com/angular/universal/issues/856
   show(id: number): Observable<{ data: any }> {
-    return this.http.get<{ data: any }>(`http://dummy.restapiexample.com/api/v1/employees`);
+    // return this.http.get<{ data: any }>(`http://dummy.restapiexample.com/api/v1/employees`);
+    return this.http.get<{ data: any }>(`https://api.sportdiary.local/users/${id}`);
   }
 }
